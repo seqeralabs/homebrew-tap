86ec6170d6077af067a58e440e3714e1fe97b799
jreleaserbot <jreleaser@kordamp.org>
2026-01-08 13:27:20 +0000

    tw v0.19.0

--- Formula/tw.rb
+++ Formula/tw.rb
@@ -1,30 +1,30 @@
-# Generated with JReleaser 1.21.0 at 2025-12-17T14:14:43.085519583Z
+# Generated with JReleaser 1.22.0 at 2026-01-08T13:26:57.257656305Z
 
 class Tw < Formula
   desc "Tower CLI"
   homepage "https://github.com/seqeralabs/tower-cli"
-  version "0.18.0"
+  version "0.19.0"
   license "MPL-2.0"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.18.0/tw-linux-x86_64", :using => :nounzip
-    sha256 "ef19b160a3f194150d864630707f8e002c07ac903f1140426690e4be8f48e37b"
+    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.19.0/tw-linux-x86_64", :using => :nounzip
+    sha256 "61992a41fdba455cece9da1044ee587930e28abd02f4d0840ce3f1aea91ed77f"
 
     def install
       bin.install "tw-linux-x86_64" => "tw"
     end
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.18.0/tw-osx-arm64", :using => :nounzip
-    sha256 "9b4acc74b48f7f4116febe40e74320f1e9d8adc24eba65b6d8ae082acae1200f"
+    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.19.0/tw-osx-arm64", :using => :nounzip
+    sha256 "0b91adea42c736f190240f7fbb83803c7950b5dd327183d4f10c50799f643ff6"
 
     def install
       bin.install "tw-osx-arm64" => "tw"
     end
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.18.0/tw-osx-x86_64", :using => :nounzip
-    sha256 "0da29aa601c087195d83bc9c9196b97c5654a4f10d3fb71ee8c1a6c60be00d21"
+    url "https://github.com/seqeralabs/tower-cli/releases/download/v0.19.0/tw-osx-x86_64", :using => :nounzip
+    sha256 "512d969cf945fe2dcb0326e309172e3d19360e446bc1635954dc7ee36d7b7229"
 
     def install
       bin.install "tw-osx-x86_64" => "tw"
@@ -34,6 +34,6 @@ class Tw < Formula
 
   test do
     output = shell_output("#{bin}/tw --version")
-    assert_match "0.18.0", output
+    assert_match "0.19.0", output
   end
 end
